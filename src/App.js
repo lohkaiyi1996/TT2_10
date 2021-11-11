34c4cd2f9e7e0c6e63f255241deb19f80607f9c5
Loh Kai Yi <lohkaiyi1996@gmail.com>
2021-11-11 12:21:43 +0800

    merge the functions the apps together

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import NewExpense from './NewExpense';
-import React, {useState} from "react";
+import React, { useState } from "react";
+import NewExpense from "./NewExpense";
+import ExpenseList from "./ExpenseList";
 
-export default function App() {
+export default function AppFunction() {
+  const [newExpense, setNewExpense] = useState({});
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newExpense.title) return;
+    //setAllExpense((prev) => [newExpense, ...prev]);
+    setNewExpense({});
+  };
+
   return (
     <main>
       <h1>Expenses</h1>
       <NewExpense
+        newExpense={newExpense}
+        handleSubmit={handleSubmit}
       />
     </main>
   );
 }
-
-

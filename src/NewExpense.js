b1ada47a99800e377497249c8fec854a005e49ed
Loh Kai Yi <lohkaiyi1996@gmail.com>
2021-11-11 12:32:47 +0800

    changes to new expenses

--- src/NewExpense.js
+++ src/NewExpense.js
@@ -2,8 +2,23 @@ import React from "react";
 
 export default function NewExpense({ newExpense, handleChange, handleSubmit }) {
   return (
+    <form onSubmit= {handleSubmit}>
+        <input name = "Expenses" 
+        placeholder = "Add new Expenses here"
+        value = {newExpense.title || ""}
+        onChange = {handleChange}
+        />
+        {!newExpense.title ? null : (
         <>
-          <button type="submit">Add New Expense</button>
+            <textarea name = "description"
+            placeholder = "Expenses Amount"
+            value = {newExpense.description || ""}
+            onChange = {handleChange}
+        /> 
+        <button type = "submit"> Add Expenses </button>
+
         </>
+      )}
+    </form>
   );
 }

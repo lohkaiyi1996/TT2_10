38d180291fc6f5f77e11360744a7c75b80c15e48
Loh Kai Yi <lohkaiyi1996@gmail.com>
2021-11-11 16:40:47 +0800

    edit function

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-export default function ExpenseList({ allExpense, handleDelete }) {
+export default function ExpenseList({ allExpense, handleDelete, editExpense }) {
   return (
     <ul>
       {allExpense.map(({ title, description, id }) => (
         <li key={id}>
           <div>
             {title}&nbsp;&nbsp;&nbsp;
-            <input type="text"/>
-            <button>Edit </button>
+            <input type="text" value={editExpense.title || ""}/>
+            {!editExpense.title ? null : (
+            <>
+            <div>
+            <textarea
+            name="description"
+            placeholder="Expense value"
+            value={editExpense.description || ""}
+            />
+          </div>
+          <button type="submit">Edit Expense</button>
+        </>
+      )}
             <button onClick={() => handleDelete(id)}>X</button>
           </div>
           {!description ? null : <p>{description}</p>}

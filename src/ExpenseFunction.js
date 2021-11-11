a2d405c6cc5328128af17e512def1d8319907341
Loh Kai Yi <lohkaiyi1996@gmail.com>
2021-11-11 12:14:46 +0800

    changes to expenses

--- src/ExpenseFunction.js
+++ src/ExpenseFunction.js
@@ -4,12 +4,6 @@ import ExpenseList from "./ExpenseList";
 
 export default function AppFunction() {
   const [newExpense, setNewExpense] = useState({});
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    setNewExpense((prev) => ({ ...prev, id: Date.now(), [name]: value }));
-  };
-
-  const [allTasks, setAllExpense] = useState([]);
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!newExpense.title) return;
